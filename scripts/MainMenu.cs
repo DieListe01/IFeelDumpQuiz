f345bb84543d9763375f28aee16c9b9da0aef6ca
DieListe01 <dirk.lindner74@gmail.com>
2026-03-29 13:25:05 +0200

    Test: Write to file in MainMenu lifecycle methods

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,11 +1,22 @@
 using Godot;
+using System.IO;
 
 public partial class MainMenu : Control
 {
+    public MainMenu()
+    {
+        File.WriteAllText("test.txt", "Constructor called");
+    }
+
+    public override void _EnterTree()
+    {
+        File.WriteAllText("test.txt", "EnterTree called");
+        base._EnterTree();
+    }
+
     public override void _Ready()
     {
-        GD.Print("MainMenu._Ready: Hello World!");
-        // Quit the application immediately to test if the scene loads
+        File.WriteAllText("test.txt", "Ready called");
         GetTree().Quit();
     }
 }
